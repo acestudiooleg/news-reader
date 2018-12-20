1e85d1ad3538d31765e53abe8fdeaf28a771ec47
Oleg Sudavnyi <acestudiooleg@gmail.com>
2018-12-20 23:14:11 +0200

    tried to add route link

--- src/app/components/NewsItem/index.tsx
+++ src/app/components/NewsItem/index.tsx
@@ -1,21 +1,41 @@
 import * as React from 'react';
 import * as style from './style.css';
 import { INewsItem } from 'app/models/NewsModel';
+import { STORE_ROUTER } from 'app/constants/stores';
+import { inject, observer } from 'mobx-react';
 
-export default (
-  { title, description, urlToImage }: INewsItem,
-  index: Number
-) => (
-  <div className={style.newsItem} key={title + index}>
-    <div className={style.thumbnail}>
-      <img src={urlToImage} alt="" />
-    </div>
-    <div className={style.content}>
-      <h2>{title}</h2>
-      <p>{description}</p>
-      <a href="/" className={style.readMore}>
-        Read More...
-      </a>
-    </div>
-  </div>
-);
+@inject(STORE_ROUTER)
+@observer
+export default class NewsItem extends React.Component {
+  title: string;
+  description: string;
+  urlToImage: string;
+  index: number;
+  constructor(props: INewsItem, index: number) {
+    super(props, index);
+    const { title, description, urlToImage } = props;
+    this.title = title;
+    this.description = description;
+    this.urlToImage = urlToImage;
+    this.index = index;
+  }
+
+  render() {
+    const { title, description, urlToImage, index } = this;
+    const router = this.props[STORE_ROUTER];
+    return (
+      <div className={style.newsItem} key={title + index}>
+        <div className={style.thumbnail}>
+          <img src={urlToImage} alt="" />
+        </div>
+        <div className={style.content}>
+          <h2>{title}</h2>
+          <p>{description}</p>
+          <a onClick={router.push('/hello')} className={style.readMore}>
+            Read More...
+          </a>
+        </div>
+      </div>
+    );
+  }
+}
